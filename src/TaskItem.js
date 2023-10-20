c3f257ea2e98c8eef4087673dc7dde8711183b29
afreenpoly <afreendeby@gmail.com>
2023-10-20 19:41:28 +0530

    null

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,20 +1,9 @@
 import React from "react";
 
-<<<<<<< HEAD
 function TaskItem({task}) {
   return (
     <div className="done">
       <input type="checkbox" name="" id="" />
-=======
-function TaskItem({ task, onTaskDone }) {
-  return (
-    <div className="done">
-      <input
-        type="checkbox"
-        name=""
-        id=""
-      />
->>>>>>> 3aabb6316b7ead50ceb3bd0b92b20b79429816c3
       <p>{task.text}</p>
     </div>
   );
